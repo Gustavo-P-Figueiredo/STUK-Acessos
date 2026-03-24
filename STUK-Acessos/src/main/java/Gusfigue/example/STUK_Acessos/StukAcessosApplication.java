584fa5e245429993b32833d458357cf17a1f723c
Gus Figue <Gustavo.pereira1204@hotmail.com>
2026-03-23 22:08:45 -0300

    Add files via upload
    Arquitetura inicial, entity Usuario e migration V1 do database

---
+++ STUK-Acessos/src/main/java/Gusfigue/example/STUK_Acessos/StukAcessosApplication.java
@@ -0,0 +1,13 @@
+package Gusfigue.example.STUK_Acessos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StukAcessosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StukAcessosApplication.class, args);
+	}
+
+}
---
+++ STUK-Acessos/src/test/java/Gusfigue/example/STUK_Acessos/StukAcessosApplicationTests.java
@@ -0,0 +1,13 @@
+package Gusfigue.example.STUK_Acessos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StukAcessosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
